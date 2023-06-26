a90b85b2316e00441228367c9dfb75dc58800b0a
Kving <kevingomezhurtado@gmail.com>
2023-06-26 22:35:55 +0200

    deploy actions test

--- variables.tf
+++ variables.tf
@@ -4,18 +4,12 @@ variable "region" {
   type        = string
 }
 
-variable "compartment_id_env" {
-  description = "Compartment ID from environment variable"
-  type        = string
-  default     = var.secrets["COMPARTMENT_ID"]
-}
-
 variable "account_secrets" {
   description = "Secrets from GitHub"
   type        = map(any)
   default = {
     #account_id     = env("ACCOUNT_ID"),
-    compartment_id = env("COMPARTMENT_ID"),
+    compartment_id = var.secrets["COMPARTMENT_ID"],
     #username       = env("USERNAME"),
   }
 }
