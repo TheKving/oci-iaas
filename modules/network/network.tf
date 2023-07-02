dd2382a578cb11d271d1926929e1192989e23c57
Kving <kevingomezhurtado@gmail.com>
2023-07-02 16:00:28 +0200

    test change name resource

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -2,6 +2,4 @@ resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
   compartment_id = var.oci_tenancy_ocid
-  display_name   = var.test_publicdns_name
-  #display_name   = "My internal VCN"
 }
