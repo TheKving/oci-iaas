74cc127b10e5e2ecac2deeae45876c14bb9b71d2
Kving <kevingomezhurtado@gmail.com>
2023-07-02 10:18:36 +0200

    test with cf_zone not created

--- modules/dns/dns_records.tf
+++ modules/dns/dns_records.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_zone" "domain" {
   account_id = var.account_id
-  zone       = var.public_domain
+  zone       = "testing.foo" #var.public_domain
 }
 resource "cloudflare_record" "www" {
   zone_id = cloudflare_zone.domain.id
